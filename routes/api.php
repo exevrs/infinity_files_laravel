6ee548a044862db5d88887e2f50738231dea25aa
Erik Palencik <erik.palencik@exe.sk>
2023-07-31 09:43:38 +0200

    update laravel

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,13 @@ require base_path('routes/files/ini.php');
 */
 
 
+$_ping = function () {
+    return response("here",200);
+};
+
+
+Route::get("/ping", $_ping);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
